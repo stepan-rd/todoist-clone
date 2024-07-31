53fee7f53dee02f61daf284d55906478ea40598d
prdel24 <brozikmrozik7@gmail.com>
2024-07-31 23:31:27 +0200

    feat: add dynamic padding to main content based on navbar width

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
+import { useState } from "react";
+import { useNavbarStore } from "./state/navbarStore";
 
 function App() {
+
+  const { navbarWidth } = useNavbarStore();
+
   return (
-    <div>
+    <div className="flex">
       <Navbar />
-      <Outlet />
+      <div style={{paddingLeft: `${navbarWidth}px`}}>
+        <Outlet />
+      </div>
     </div>
   );
 }
