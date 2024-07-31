4461627315cd2fc2176a9dada5c645c4e4717588
prdel24 <brozikmrozik7@gmail.com>
2024-07-31 16:58:45 +0200

    build:  add new font to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "SFpro": ["SF Pro", "Arial"]
+      }
+    },
   },
   plugins: [],
 }
