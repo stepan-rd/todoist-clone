90c50c371f7fc935e2b4fb1267ab99aa7a2eac79
prdel24 <brozikmrozik7@gmail.com>
2024-08-01 09:10:46 +0200

    feat: add routes to inbox and upcoming page

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -1,6 +1,8 @@
 import App from "@/App";
 import { ErrorPage } from "@/pages/ErrorPage/ErrorPage";
+import { InboxPage } from "@/pages/InboxPage/InboxPage";
 import { TodayTasksPage } from "@/pages/TodayTasksPage/TodayTasksPage";
+import { UpcomingPage } from "@/pages/UpcomingPage/UpcomingPage";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 
 export const router = createBrowserRouter([
@@ -11,6 +13,8 @@ export const router = createBrowserRouter([
     children: [
       { index: true, element: <Navigate to="/app/today" /> },
       { path: "today", element: <TodayTasksPage /> },
+      { path: "inbox", element: <InboxPage /> },
+      { path: "upcoming", element: <UpcomingPage /> },
     ],
   },
 ]);
