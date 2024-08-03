69ca7b90df0ffc19f8411b1674c24bfbcb01fda0
prdel24 <brozikmrozik7@gmail.com>
2024-08-03 22:56:46 +0200

    feat: create zustand store tasksStore
    store to store all state related to tasks

--- src/state/tasksStore.tsx
+++ src/state/tasksStore.tsx
@@ -3,9 +3,12 @@ import { create } from "zustand";
 
 type TasksStoreType = {
   todayTasks: TaskType[];
-  setTodayTasks: (val: TaskType[]) => void;
+  setTodayTasks: (val: TaskType[] | ((prev: TaskType[]) => TaskType[])) => void;
 };
 export const useTasksStore = create<TasksStoreType>((set) => ({
   todayTasks: [],
-  setTodayTasks: (val) => set((state) => ({ todayTasks: val })),
+  setTodayTasks: (val) =>
+    set((state) => ({
+      todayTasks: typeof val === "function" ? val(state.todayTasks) : val,
+    })),
 }));
