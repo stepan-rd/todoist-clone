492742c4e6c0cb409e2cf568d7f518f84cea5951
prdel24 <brozikmrozik7@gmail.com>
2024-08-03 22:42:25 +0200

    refactor: update navbar and padding state logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
-import { useState } from "react";
 import { useNavbarStore } from "./state/navbarStore";
+import { useCallback, useEffect, useState } from "react";
+import { Overlay } from "./components/Overlay";
 
 function App() {
+  const {
+    navbarHidden,
+    setNavbarHidden,
+    navbarMdDevicesHidden,
+    setNavbarMdDevicesHidden,
+  } = useNavbarStore();
 
-  const { navbarWidth } = useNavbarStore();
+  const [paddingLeft, setPaddingLeft] = useState("0px");
+
+  useEffect(() => {
+    function getPaddingLeft() {
+      if (navbarHidden) return "0px";
+      else if (navbarMdDevicesHidden === false) return "0px";
+
+      return "210px";
+    }
+
+    const paddingLeft = getPaddingLeft();
+
+    setPaddingLeft(paddingLeft);
+  }, [navbarHidden, navbarMdDevicesHidden]);
 
   return (
     <div className="flex">
       <Navbar />
-      <div style={{paddingLeft: `${navbarWidth}px`}}>
+      <div
+        className="w-full transition-all duration-300"
+        style={{ paddingLeft: paddingLeft }}
+      >
+        {navbarMdDevicesHidden === false && (
+          <Overlay
+            onClick={() => {
+              setNavbarHidden(true);
+              setNavbarMdDevicesHidden(true);
+            }}
+          />
+        )}
         <Outlet />
       </div>
     </div>
